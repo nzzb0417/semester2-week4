322a7fa26fbbf3acb624c64a4f5d50996d3d0ebb
nzzb0417 <nzzb0417@leeds.ac.uk>
2026-08-07 00:49:39 +0100

    Update makeString.c

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,19 +1,26 @@
-
 /*
-Name:
-Student ID:
+Name: Abdimalik Hussein
+Student ID: 202028878
 */
 
 #include <stdio.h>
 #include <string.h>
 
-int main( int argc, char **argv ) {
+int main(int argc, char **argv) {
 
     // define appropriate data to hold your answer
+    char buffer[51] = "";
 
     // process the command-line data using appropriate string functions
+    for (int i = 1; i < argc; i++) {
+        strcat(buffer, argv[i]);
 
-    printf("%s\n",buffer); // use only this single print statement in your submitted code
+        if (i < argc - 1) {
+            strcat(buffer, "-");
+        }
+    }
+
+    printf("%s\n", buffer); // use only this single print statement in your submitted code
 
     return 0;
 }
